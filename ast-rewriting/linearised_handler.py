27adaf4fda3194469b39bb9b3f0f06615a913729
Louis Maddox <louismmx@gmail.com>
2022-01-30 22:54:46 +0000

    Demo completed for first refactor rule (kept intermediate files)

--- ast-rewriting/linearised_handler.py
+++ ast-rewriting/linearised_handler.py
@@ -3,5 +3,8 @@ from __future__ import annotations
 
 def ternary_assigned_handler(event: dict) -> int | None:
     payload = event.get("payload")
-    output = None if payload is None else len(payload)
+    if payload is None:
+        payload = None
+    else:
+        payload = len(payload)
     return output
